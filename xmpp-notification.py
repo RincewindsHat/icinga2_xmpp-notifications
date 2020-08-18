93b1a1a6b995252d4d99523609d7086c53cd4678
Lorenz Kästle <lorenz.kaestle@netways.de>
2020-08-18 18:55:33 +0200

    More stuff.. needs work

--- xmpp-notification.py
+++ xmpp-notification.py
@@ -7,32 +7,8 @@ import argparse
 import ssl
 
 def usage():
-    print("XMPP Notification sender for icinga2\n\n"
-            "Usage:\n"
-            "xmpp-notification.py\n"
-            "   -h|--help   (Prints this help)\n"
-            "   --service|--host (MUST be first argument and decides, whether this "
-            "is a host or a service notification)\n\n"
-
-            "Obligatory arguments:\n"
-            "   -r|--recipient RECIPIENT ...    (JID of the recipient)\n"
-            "   -f|--sender SENDER     (XMPP-Account (JID) to send from)\n"
-            "   -p|--password PASSWORD   (Login password for the sending XMPP account)\n"
-            "   -d|--notification_date DATE (Date of the occuring event ($icinga.long_date_time$))\n"
-            "   -N|--host HOST   (The host object to which the event is attached ($host.name$))\n"
-            "   -t|--notification_type TYPE\n"
-            "   [-n|--hostdisplayname HOSTDISPLAYNAME] Pretty name for the host ($host.display_name$)\n\n"
-            "Host notification options:\n"
-            "   -h|--hoststate STATE ($host.state$)\n\n"
-            "Service notification options:\n"
-            "   -e|--servicename SERVICE    ($service.name$)\n"
-            "   [-o|--serviceoutput]  OUTPUT (Check result or something like that($service.output$))\n"
-            "   -s|--servicestate STATE ($service.state$)\n"
-            "   [-u|--servicedisplayname SERVICEDISPLAYNAME] ($service.display_name$)"
-            )
+    pass
         
-
-
 class SendMsgBot(sleekxmpp.ClientXMPP):
 
     def __init__(self, recipient, msg, sender_jid, password):
@@ -68,10 +44,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Send XMPP Notifications")
 
     # Always necessary arguments
+
+    parser.add_argument('-m', '--mode',
+                              choices=['host', 'service'],
+                              help="Choose if this is a host or service notification",
+                              required=True)
+
     parser.add_argument('-r', '--recipient', 
-                        type=str, 
+                        type=str,
                         help="The JID(s) of the recipient(s)",
-                        nargs=1,
+                        nargs='+',
                         action='append',
                         required=True)
 
@@ -87,21 +69,21 @@ if __name__ == "__main__":
                         help="XMPP login password",
                         required=True)
 
-    parser.add_argument('-d', '--notification_date',
+    parser.add_argument('-D', '--notification_date',
                         type=str,
                         nargs=1,
                         help="The date of the notification",
                         required=True)
 
-    parser.add_argument('-N', '--host',
+    parser.add_argument('-H', '--hostname',
                         type=str,
                         nargs=1,
                         help="The respective host",
                         required=True)
 
-    parser.add_argument('-n', '--hostdisplayname',
+    parser.add_argument('-d', '--hostdisplayname',
                         type=str,
-                        nargs='*',
+                        nargs=1,
                         help="Name of the host")
 
     parser.add_argument('-t', '--notification_type',
@@ -112,51 +94,46 @@ if __name__ == "__main__":
     # Optional
     parser.add_argument('-a', '--notification_author',
                         type=str,
-                        nargs='*',
+                        nargs=1,
                         help="The service sending this")
 
-    if sys.argv[1] == "--host":
-        is_host_notification = True
-        # Host notification
-        parser.add_argument('-S', '--hoststate',
-                            type=str, 
-                            nargs=1,
-                            required=True,
-                            help="State of the service")
+    parser.add_argument('-c', '--notification_Comment',
+                        type=str,
+                        nargs=1,
+                        help="Some comment")
 
-    elif sys.argv[1] == "--service":
-        is_host_notification = False
-        parser.add_argument('-e','--servicename',
-                            type=str,
-                            nargs='*',
-                            required=True,
-                            help="Name of the service in question")
+    parser.add_argument('args',
+                        nargs=argparse.REMAINDER)
 
-        parser.add_argument('-o', '--serviceoutput',
-                            type=str,
-                            nargs='*',
-                            required=True,
-                            help="Result of the service check")
+    parser.add_argument('-S', '--state',
+                        type=str, 
+                        nargs=1,
+                        required=True,
+                        help="State of the host/service")
+    parser.add_argument('-O', '--output',
+                        type=str, 
+                        nargs=1,
+                        required=True,
+                        help="Output of the host/service check")
 
-        parser.add_argument('-S', '--servicestate',
-                            type=str, 
-                            nargs=1,
-                            required=True,
-                            help="State of the service")
+    parser.add_argument('-e','--servicename',
+                        type=str,
+                        nargs=1,
+                        required=False,
+                        help="Name of the service in question")
+
+    parser.add_argument('-u', '--servicedisplayname',
+                        type=str,
+                        nargs=1,
+                        help="Display name of the service")
+
+    args = parser.parse_args(args.args)
 
 
-        parser.add_argument('-u', '--servicedisplayname',
-                            type=str,
-                            nargs='*',
-                            help="Display name of the service")
-    else:
-        usage()
-        sys.exit(2)
-        
-    args = parser.parse_args(sys.argv[2:])
-
-    if is_host_notification == False:
+    if args.mode == 'service':
         # Service notification
+        if args.servicename is None:
+            print("No servicename for a service notification")
         if args.servicedisplayname is not None:
             service = args.servicedisplayname[0]
         else:
@@ -167,12 +144,9 @@ if __name__ == "__main__":
         else:
             host = args.host[0]
 
-        text = "{0} {1}\n{2}: {3}\n{4}".format(args.servicestate[0],
-                                         service,
-                                         host,
-                                         args.serviceoutput[0],
-                                         args.notification_date[0]
-                                        )
+        title = f"{service} on {host} is {servicestate[0]}"
+        text = f"{title}\nOutput: {ags.serviceoutput[0]}\nTime/Date: {args.notification_date}"
+
     else:
         # Host notification
         if args.hostdisplayname is not None:
@@ -180,11 +154,8 @@ if __name__ == "__main__":
         else:
             host = args.host[0]
 
-        text = "{0}:\nHost {1} is {2}\n{3}".format(args.notification_type[0],
-                                        host,
-                                        args.hoststate[0],                                    
-                                        args.notification_date[0]
-                                        )
+        title = f"Host {host} is {args.hoststate[0]}"
+        text = f"{title}:\nOutput: {args.hostoutput[0]}\nTime/Date: {args.notification_date}"
 
     #print(args)
     for i in args.recipient:
